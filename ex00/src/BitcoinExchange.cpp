6c78a80e69c8fcc0e2093863b85e97b04de0a1a7
bolsakova <bolsakova@github.com>
2026-06-27 20:19:08 +0200

    ex00: BitcoinExchange.cpp - finished realization of constructors, destructor and assignment operator

--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -1,3 +1,45 @@
 #include "../inc/BitcoinExchange.hpp"
 
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+/**
+ * @brief Creates a BitcoinExchange object using the default database file.
+ * @throws std::runtime_error If the default database file cannot be opened.
+ */
+BitcoinExchange::BitcoinExchange() {
+	loadDatabase("data.csv");
+}
+
+/**
+ * @brief Creates a BitcoinExchange object using a custom database file.
+ * @param databaseFile Path to the CSV database file.
+ * @throws std::runtime_error If the database file cannot be opened.
+ */
+BitcoinExchange::BitcoinExchange(const std::string& databaseFile) {
+	loadDatabase(databaseFile);
+}
+
+/**
+ * @brief Copy constructor.
+ * @param other Object to copy from.
+ */
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) : _database(other._database) {}
+
+
+/**
+ * @brief Destructor.
+ */
+BitcoinExchange::~BitcoinExchange() {}
+
+
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other) {
+	if (this != &other)
+		_database = other._database;
+	return *this;
+}
 
