56b3a14d34cf41ebb2310e881a50e9d25b03891b
bolsakova <bolsakova@github.com>
2026-06-27 20:27:45 +0200

    ex00: BitcoinExchange.cpp - finished realization of private method that loads Bitcoin exchange rates from a CSV database file.

--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -29,17 +29,46 @@ BitcoinExchange::BitcoinExchange(const std::string& databaseFile) {
  */
 BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) : _database(other._database) {}
 
-
 /**
  * @brief Destructor.
  */
 BitcoinExchange::~BitcoinExchange() {}
 
-
-
+/**
+ * @brief Copy assignment operator.
+ * @param other Object to copy from.
+ * @return Reference to the current object.
+ */
 BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other) {
 	if (this != &other)
 		_database = other._database;
 	return *this;
 }
 
+/**
+ * @brief Loads Bitcoin exchange rates from a CSV database file.
+ * 
+ * The file is expected to contain lines in the following format:
+ * 	- date,exchange_rate
+ * The first line is treated as a header and skipped. Every other line is
+ * parsed and inserted into the internal std::map.
+ * 
+ * @param filename Path to the CSV database file.
+ * @throws std::runtime_error If the file cannot be opened
+ */
+void BitcoinExchange::loadDatabase(const std::string& filename) {
+	std::ifstream file(filename);
+
+	if (!file.is_open())
+		throw std::runtime_error("Error: could not open database.");
+
+	std::string line;
+	std::getline(file, line);
+
+	while (std::getline(file, line)) {
+		if (line.empty())
+			continue;
+		std::pair<std::string, double> data = parseDatabaseLine(line);
+		_database[data.first] = data.second;
+	}
+}
