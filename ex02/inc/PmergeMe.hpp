d003dda0c37daaf16598da1788b7cbcc577e2910
bolsakova <bolsakova@github.com>
2026-06-28 19:53:25 +0200

    ex02: copied architecture from ex01, created necessary files to start working on this exercise

---
+++ ex02/inc/PmergeMe.hpp
@@ -0,0 +1,13 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+class PmergeMe {
+	private:
+	public:
+		PmergeMe();
+		PmergeMe(const PmergeMe& other);
+		~PmergeMe();
+		PmergeMe& operator=(const PmergeMe& other);
+};
+
+#endif
---
+++ ex02/src/main.cpp
@@ -0,0 +1,14 @@
+#include "../inc/PmergeMe.hpp"
+
+/**
+ * @brief Entry point of the PmergeMe program.
+ * 
+ * The program expects exactly one command-line argument.
+ * 
+ * @param ac Number of command-line arguments.
+ * @param av Array of command-line arguments.
+ * @return 0 on success, 1 on error.
+ */
+int main(int ac, char *av[]) {
+	return 0;
+}
