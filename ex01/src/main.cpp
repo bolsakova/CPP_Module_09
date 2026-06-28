826e3a1f3d106ec350c4d3c9b33aa3cbaa8b618d
bolsakova <bolsakova@github.com>
2026-06-28 18:39:54 +0200

    ex01: main.cpp - finished entry point of the RPN program

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,7 +1,33 @@
 #include "../inc/RPN.hpp"
 
+#include <exception>
+#include <iostream>
+
 /**
  * @brief Entry point of the RPN program.
+ * 
+ * The program expects exactly one command-line argument containing
+ * the Reverse Polish Notation expression.
+ * 
+ * @param ac Number of command-line arguments.
+ * @param av Array of command-line arguments.
+ * @return 0 on success, 1 on error.
  */
-int main() {
+int main(int ac, char *av[]) {
+	if (ac != 2) {
+		std::cerr << "Error" << std::endl;
+		return 1;
+	}
+
+	try {
+		RPN rpn;
+		int res = rpn.evaluate(av[1]);
+
+		std::cout << res << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+
+	return 0;
 }
